1609dac17e761df8ab0eb1aaeeedf8408e550bee
Adrien-OLR <aolivier@edenschool.fr>
2018-09-24 15:58:43 +0200

    folder scss

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import logohxh from "./images/Hunter_Association_logo.png";
-import './App.css';
+import './scss/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="header">
-        <img className="logohxh" src={logohxh} />
+        <img className="logohxh" src={logohxh} alt="logohxh" />
         </header>
       </div>
     );
