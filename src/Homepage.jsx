11da4e6aeb646d98df0e277b7904aae216398777
mathiasestdejapris <mmiraglio@edenschool.fr>
2018-09-27 17:46:39 +0200

    homepage

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom"
+import hxh from "./images/hxhlogo.png";
 
 class Homepage extends Component {
   render() {
     return (
-      <div>
-      <ul>
-      <li>  <Link to="brigade"> <button> brigade </button></Link> </li>
-      <li>  <Link to="heros"> <button> hero </button></Link> </li>
+      <section className="homepage">
+      <img className="hxh" src={hxh}></img>
+      <ul className="display">
+      <li>  <Link to="brigade"> <div class="button">Brigade</div></Link> </li>
+      <li>  <Link to="heros"> <div class="button">Heros </div></Link> </li>
+      <li>  <Link to=""> <div class="button">Kimera Ants</div></Link> </li>
+
         </ul>
-      </div>
+      </section>
     );
   }
 }
